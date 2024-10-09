f393e0413e674eef806e27fc946833d3cfb1a8dd
AryanshDev <aryanshappmaker@gmail.com>
2024-10-09 08:03:58 +0530

    now check

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,7 @@ const sharingRouter = require("./routes/Sharing");
 const helmet = require("helmet");
 const chatServer = require("http").createServer(app);
 const cors = require("cors");
+const cookieParser = require("cookie-parser")
 const Groq = require("groq-sdk");
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
@@ -63,6 +64,7 @@ io.on("connection", (socket) => {
 io.on("disconnection", () => {});
 
 app.use(bodyParser.json());
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   session({
