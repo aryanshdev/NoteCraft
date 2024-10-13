074f1d39324ad10900d348d4ee7d8ff76980ffbf
AryanshDev <aryanshappmaker@gmail.com>
2024-10-13 21:22:58 +0530

    Added AI Mention Button in Chat

--- Frontend/src/components/ChatSection.jsx
+++ Frontend/src/components/ChatSection.jsx
@@ -32,7 +32,6 @@ function ChatSection({ id, openFunction }) {
   }, []);
 
   useEffect(() => {
-
     socket.on("SYSTEM", (msg) => {
       setMessages((prevMessages) => [...prevMessages, ["SYSTEM", msg]]);
     });
@@ -166,10 +165,27 @@ function ChatSection({ id, openFunction }) {
             </div>
 
             {/* Message Sending Area */}
-            <div className="flex w-full flex-row gap-3 items-center">
+            <div className="flex w-full flex-row items-center">
+              <button  className="  bg-gray-500 bg-opacity-35 px-2 h-full  rounded-md rounded-r-none"
+                onClick={() => {
+                  document.getElementById("chatMSG").value = "@NC-AI ";
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  class="size-6 fill-blue-400"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </button>
               <input
                 type="text"
-                className=" w-full bg-white h-fit bg-opacity-5 px-4 py-2 outline-none focus:outline-none rounded-md"
+                className=" w-full bg-white h-fit bg-opacity-5 px-4 py-2 outline-none focus:outline-none rounded-md rounded-l-none"
                 id="chatMSG"
                 onKeyDown={(key) => {
                   key.key == "Enter" ? sendMessage() : null;
