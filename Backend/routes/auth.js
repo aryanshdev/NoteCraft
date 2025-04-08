768117c3053869f61f2a18c13b8227226206234e
AryanshDev <aryanshappmaker@gmail.com>
2025-04-08 15:10:04 +0530

    Local To Normal Host

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:10000/auth/google/process-login",
+      callbackURL: "https://notecraftai-xct5.onrender.com/auth/google/process-login",
     },
     async (accessToken, refreshToken, profile, done) => {
       const userData = await usersCol.findOne({ email: profile._json.email });
@@ -59,7 +59,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:10000/auth/github/process-login",
+      callbackURL: "https://notecraftai-xct5.onrender.com/auth/github/process-login",
       scope: ["user:email"],
     },
     async (accessToken, refreshToken, profile, done) => {
@@ -118,7 +118,7 @@ router.get(
   "/google/process-login",
   passport.authenticate("google", {
     session: false,
-    failureRedirect: "http://localhost:5173/login",
+    failureRedirect: "https://notecraft-ai.onrender.com/login",
   }),
   async function (req, res) {
     console.log(req.user)
@@ -128,7 +128,7 @@ router.get(
       maxAge: 24 * 60 * 60 * 1000 * 30,
     }
     );
-    res.redirect("http://localhost:5173/dashboard");
+    res.redirect("https://notecraft-ai.onrender.com/dashboard");
   }
 );
 
@@ -138,7 +138,7 @@ router.get(
   "/github/process-login",
   passport.authenticate("github", {
     session: false,
-    failureRedirect: "http://localhost:5173/login",
+    failureRedirect: "https://notecraft-ai.onrender.com/login",
   }),
   async function (req, res) {
     res.cookie("_uid", req.user,{
@@ -146,7 +146,7 @@ router.get(
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000 * 30,
     });
-    res.redirect("http://localhost:5173/dashboard");
+    res.redirect("https://notecraft-ai.onrender.com/dashboard");
   }
 );
 
