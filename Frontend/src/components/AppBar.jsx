f3ff12f23d832f3e18819795c1ed7d9db77f0e1f
AryanshDev <aryanshappmaker@gmail.com>
2025-06-09 00:09:58 +0530

    Fixed LogOut Link

--- Frontend/src/components/AppBar.jsx
+++ Frontend/src/components/AppBar.jsx
@@ -16,7 +16,7 @@ function AppBar() {
     showHideSideBar();
   };
   const logout = () => {
-    fetch("/logout", {
+    fetch("https://notecraftai-xct5.onrender.com/logout", {
       method: "POST",
       credentials: "include", // Necessary for cookies
     }).then((res) => {
